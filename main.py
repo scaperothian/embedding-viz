4add7cb104791b0b2afd55330c46f616a817a322
John Doe <scaperoth@berkeley.edu>
2023-11-26 22:18:06 -0500

    tweaks to make sure the deps work ok.

--- main.py
+++ main.py
@@ -2,18 +2,45 @@ from flask import Flask, render_template, jsonify
 from os import path
 import json
 import pickle 
+import pandas as pd
 
 app = Flask(__name__)
 root_path = path.dirname(__file__)
 
-import embeddings.TextEmbedding as TextEmbedding
+from embeddings import TextEmbeddings,download
 
-file = "podcast_show_description_embeddings.pkl"
-with open(file, 'rb') as f:
-    showemb = pickle.load(f)
+file = "podcast_show_description_embeddings.20231126.pkl"
+filename = 'metadata_with_episode_dates_and_category.tsv'
+data_key='show_description'
+label_key='show_name'
+    
+try: 
+    df = pd.read_csv(filename,sep='\t')
+except Exception as e1:
+#print(e)
+    try: 
+        print("Attempt to pull from Google Drive")
+        id = "1guz7ILFUGLN2aYoXUez-K5ZCw0Xjo6m4"
+        download(id,filename)
+        df = pd.read_csv(filename,sep='\t')
+    except Exception as e2:
+        print("Fetch to Google Drive failed to download file.")
+        print(e1, e2)
+        exit()
 
-emb = TextEmbedding()
-emb.load(showemb)
+# clean the data
+df['release_date'] = pd.to_datetime(df['release_date'], format='%Y-%m-%d').reset_index(drop=True)
+df = df[~df['release_date'].isna()]
+df = df[~df['category'].isna()]
+df = df[~df['show_description'].isna()]
+df = df[~df['show_name'].isna()]
+
+df_shows = df.drop_duplicates(['show_name','show_description'])[['show_name','show_description']].reset_index(drop=True)
+
+
+
+emb = TextEmbeddings()
+emb.load(file, df_shows)
 
 
 @app.route('/')
