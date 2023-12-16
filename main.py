083e7e9ae6fcf65fdc1f4264f155de65f107fab8
scaperoth@berkeley.edu <scaperoth@berkeley.edu>
2023-12-16 06:55:44 -0500

    simple style updates.  getting the user input displayed

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def index():
     if request.method == "POST":
         user_input = request.form["input_text"]
         similar_strings = find_similarity(user_input) # Replace with your actual function
-        return render_template("index.html", similar_strings=similar_strings)
+        return render_template("index.html", similar_strings=similar_strings, user_input=user_input)
     return render_template("index.html")
 
 if __name__ == "__main__":
